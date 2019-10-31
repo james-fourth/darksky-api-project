09fc62dccdd04bd6e9aa2eaebd0b31d116d6a8fb
James J Potts IV <james@pottsiv.com>
2019-10-31 11:56:15 -0700

    assemble quick and dirty weather app

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 /*jshint esversion: 6*/
 
+require('dotenv').config()
 const express = require("express");
+const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
@@ -15,9 +19,16 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
+	const latitude = req.body.lat;
+	const longitude = req. body.long;
+	const url = "https://api.darksky.net/forecast/" + process.env.DARK_SKY_SECRET + "/" + latitude + "," + longitude;
+
+	options = {
+		uri: url
+	};
 
 	request(options, function(error, response, body) {
-			
+		res.send(JSON.parse(body));
 	});
 });
 
