3211bdb2b0708f12a947a390e644df9f1a7e1e4a
James J Potts IV <james@pottsiv.com>
2019-10-31 00:48:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6*/
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+	request(options, function(error, response, body) {
+			
+	});
+});
+
+app.listen(3000, function() {
+	console.log("Server is listening on 3000");
+});
