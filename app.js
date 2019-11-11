5102acf7ae1b247f0daaf26c5b73da56a603ce2f
James J Potts IV <james@pottsiv.com>
2019-11-11 11:37:23 -0700

    Define port env variable for Heroku

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ app.post("/", function(req, res) {
 	});
 });
 
-app.listen(3000, function() {
-	console.log("Server is listening on 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() { console.log("Server is listening on port 3000")});
